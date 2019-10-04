1b83a322b5af8b93faf275759098b4b1052736a0
Ewerton Marques <marquescpv@gmail.com>
2019-10-04 00:38:37 -0300

    Aula 4 concluida

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -3,6 +3,31 @@ class LivroDao{
         this._db = db;
     }
 
+    adiciona(livro){
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo,
+                    preco,
+                    descricao
+                ) values (?,?,?)
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao
+            ],
+            function(err){
+                if(err){
+                    console.log(err);
+                    return reject('Não foi possivel adicionar o livro');
+                }
+                resolve();
+            }
+            )  
+        });
+    }
+
     lista(){
         return new Promise((resolve, reject) => {
             this._db.all(
